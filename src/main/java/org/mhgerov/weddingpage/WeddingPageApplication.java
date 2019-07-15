f8fc153ebc1ae911984d7fda534207850d8d5816
Michael Gerov <michael.gerov@firstdata.com>
2019-07-15 15:12:19 -0400

    init

---
+++ src/main/java/org/mhgerov/weddingpage/WeddingPageApplication.java
@@ -0,0 +1,13 @@
+package org.mhgerov.weddingpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeddingPageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mhgerov/weddingpage/WeddingPageApplicationTests.java
@@ -0,0 +1,16 @@
+package org.mhgerov.weddingpage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeddingPageApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
