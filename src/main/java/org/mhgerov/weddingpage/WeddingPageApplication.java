ed0e6e3ca96a80f3c14e7052c8a3ad26474bb5b8
Michael Gerov <michael.gerov@firstdata.com>
2019-07-15 15:24:37 -0400

    integration http imported

--- src/main/java/org/mhgerov/weddingpage/WeddingPageApplication.java
+++ src/main/java/org/mhgerov/weddingpage/WeddingPageApplication.java
@@ -2,8 +2,10 @@ package org.mhgerov.weddingpage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:spring-integration.xml")
 public class WeddingPageApplication {
 
     public static void main(String[] args) {
