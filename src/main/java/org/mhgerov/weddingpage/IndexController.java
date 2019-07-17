4c380356a50c3e1fd30537d53a2269a44622a23c
Michael Gerov <michael.gerov@firstdata.com>
2019-07-17 01:30:06 -0400

    rel paths set

--- src/main/java/org/mhgerov/weddingpage/IndexController.java
+++ src/main/java/org/mhgerov/weddingpage/IndexController.java
@@ -1,17 +1,20 @@
 package org.mhgerov.weddingpage;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class IndexController {
 
-    @GetMapping
+    @Value("${org.mhgerov.url}")
+    String url;
+
+    @GetMapping("/")
     public String getIndex(Model model) {
-        model.addAttribute("name","Baird");
+        model.addAttribute("url",url);
         return "index";
     }
 }
