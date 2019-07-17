13d248b948bb1a0714b192ea98f8af9210bb99cb
Michael Gerov <michael.gerov@firstdata.com>
2019-07-17 00:55:58 -0400

    Template engine works

--- src/main/java/org/mhgerov/weddingpage/IndexController.java
+++ src/main/java/org/mhgerov/weddingpage/IndexController.java
@@ -1,16 +1,17 @@
 package org.mhgerov.weddingpage;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
 
     @GetMapping
-    public String getIndex() {
+    public String getIndex(Model model) {
+        model.addAttribute("name","Baird");
         return "index";
     }
 }
