dfb45a1d8683b959f1d491853e22e7c0ee16f7cd
Michael Gerov <michael.gerov@firstdata.com>
2019-07-17 00:43:23 -0400

    imported static resources

---
+++ src/main/java/org/mhgerov/weddingpage/IndexController.java
@@ -0,0 +1,16 @@
+package org.mhgerov.weddingpage;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public String getIndex() {
+        return "index";
+    }
+}
--- src/main/java/org/mhgerov/weddingpage/WeddingPageApplication.java
+++ src/main/java/org/mhgerov/weddingpage/WeddingPageApplication.java
@@ -3,13 +3,23 @@ package org.mhgerov.weddingpage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-@ImportResource("classpath:spring-integration.xml")
+//@ImportResource("classpath:spring-integration.xml")
 public class WeddingPageApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WeddingPageApplication.class, args);
+    }
+
+    @Controller
+    public class RootController {
+
+
+
     }
 
 }
