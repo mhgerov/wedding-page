61945ba25a2b0ff259a943ea34565078399fc080
Michael Gerov <michael.gerov@firstdata.com>
2019-07-25 16:05:44 -0400

    ch ch ch changes!

--- src/main/java/org/mhgerov/weddingpage/orm/RegistryItem.java
+++ src/main/java/org/mhgerov/weddingpage/orm/RegistryItem.java
@@ -1,22 +1,28 @@
 package org.mhgerov.weddingpage.orm;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data @AllArgsConstructor
 public class RegistryItem {
 
-    public String imgSrc;
-    public String title;
-    public String subText;
-    public float cost;
-    public float progress;
+    String id;
+    String imgSrc;
+    String title;
+    String subText;
+    float cost;
+    float progress;
 
-    public String costF() {return String.format("$%.2f",this.cost);}
-    public String progressF() {return String.format("$%.2f",this.progress);}
-    public String percentageF() {return String.format("%.2f%%",100*progress/cost);}
-
-    public RegistryItem(String imgSrc, String title, String subText, float cost, float progress) {
-        this.imgSrc = imgSrc;
-        this.title = title;
-        this.subText = subText;
-        this.cost = cost;
-        this.progress = progress;
+    public String costF() {
+        return String.format("$%.2f", this.cost);
     }
+
+    public String progressF() {
+        return String.format("$%.2f", this.progress);
+    }
+
+    public String percentageF() {
+        return String.format("%.2f%%", 100 * progress / cost);
+    }
+
 }
--- src/main/java/org/mhgerov/weddingpage/si/RegistryItemsEnricher.java
+++ src/main/java/org/mhgerov/weddingpage/si/RegistryItemsEnricher.java
@@ -3,6 +3,7 @@ package org.mhgerov.weddingpage.si;
 import org.mhgerov.weddingpage.orm.RegistryItem;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -11,8 +12,9 @@ import java.util.Map;
 public class RegistryItemsEnricher {
 
     public Map addRegistryItems(Map payload) {
-        List<RegistryItem> registryList = Arrays.asList(new RegistryItem("registry_cat.jpg","A THIRD Cat!", "Kitty & Beaker need a new friend...",500.0f, 301.25f));
-
+        List<RegistryItem> registryList = new ArrayList<RegistryItem>();
+        registryList.add(new RegistryItem("1","registry_cat.jpg","A THIRD Cat!", "Kitty & Beaker need a new friend...",500.0f, 301.25f));
+        registryList.add(new RegistryItem("2", "registry_grill.jpg","Weber Gas Grill","For hosting our favorite donaters!",999f,0f));
         payload.put("registryList",registryList);
         return payload;
 
