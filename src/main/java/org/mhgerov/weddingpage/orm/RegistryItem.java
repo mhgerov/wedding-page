ee4fb9d8f79f5a5eb77811a35b2f44c5c0a52b97
Michael Gerov <michael.gerov@firstdata.com>
2019-07-28 13:07:15 -0400

    registry list moved to PayPal money pools

--- src/main/java/org/mhgerov/weddingpage/orm/RegistryItem.java
+++ src/main/java/org/mhgerov/weddingpage/orm/RegistryItem.java
@@ -6,23 +6,9 @@ import lombok.Data;
 @Data @AllArgsConstructor
 public class RegistryItem {
 
-    String id;
     String imgSrc;
     String title;
     String subText;
-    float cost;
-    float progress;
-
-    public String costF() {
-        return String.format("$%.2f", this.cost);
-    }
-
-    public String progressF() {
-        return String.format("$%.2f", this.progress);
-    }
-
-    public String percentageF() {
-        return String.format("%.2f%%", 100 * progress / cost);
-    }
+    String href;
 
 }
